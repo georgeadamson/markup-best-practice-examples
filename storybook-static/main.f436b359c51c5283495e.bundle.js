588a388cca134d9aae125f4feb3d7fe788830ca2
George Adamson <george@adamson.com>
2019-09-27 15:42:45 +0100

    Chore: added storybook-static

---
+++ storybook-static/main.f436b359c51c5283495e.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(419),c(527),n.exports=c(900)},1:function(n,o){},527:function(n,o,c){"use strict";c.r(o);c(528),c(691),c(872)}},[[0,1,2]]]);
